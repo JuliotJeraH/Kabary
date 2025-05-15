8370b48028eeeba487f8e62bc7e4bb7cd26246ea
JuliotJeraH <juliotjera7@gmail.com>
2025-05-15 14:27:06 +0200

    Last commit by Juliot

--- Mpikabary.java
+++ Mpikabary.java
@@ -19,10 +19,10 @@ public class Mpikabary {
         int point = 0;
         for (int i = 0; i < this.kabary.length(); i++) {
             if (this.kabary.charAt(i) == '.') {
-                // Check if it's a single '.' and not part of "..."
+                // 
                 if (i + 2 < this.kabary.length() && this.kabary.charAt(i + 1) == '.' && this.kabary.charAt(i + 2) == '.') {
                     point++;
-                    i+=3; // Skip the next two '.' characters
+                    i+=3; // 
                 } else {
                     point++;
                 }
@@ -36,26 +36,24 @@ public class Mpikabary {
 
     public int isaTeny() {
         int mot = 0;
-        boolean inWord = false;
         for (int i = 0; i < kabary.length(); i++) {
             char c = kabary.charAt(i);
-            if (Character.isLetterOrDigit(c)) {
-                inWord = true;
-            } else if ((c == ' ' || c == ',' || c == ';') && inWord) {
+
+            if((c == ' ' || c == ',' || c == ';')) {
                 mot++;
-                inWord = false;
             }
         }
-        if (inWord) {
-            mot++; // Count the last word if it wasn't followed by a delimiter
+        if (kabary.charAt(kabary.length() - 1) == ' ') {
+            mot--;
         }
-        return mot;
+        
+        return mot + 1;
     }
 
     public int alavankabary(int Point, int mot, int vitesse) {
-        int alavankabary = mot * vitesse;
+        int alavankabary = mot / vitesse;
         return (alavankabary + Point) * 60;
-        
+     
     }
 
 }
